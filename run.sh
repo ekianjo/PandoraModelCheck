e575be0fbc5c6eb8a8e8c81265b569b2bbfc69bd
ekianjo <ekianjo@gmail.com>
2013-04-23 00:31:08 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-echo "caca"
+# get the firmware information
 var=$(cat /etc/op-version | sed -n 4p)
 var0="Your current firmware is $var."
-var2=$(cat /proc/pandora/sys_mhz_max)
 
+# get the max CPU syspeed to confirm whether it's a old OMAP or a 1Ghz OMAP.
+var2=$(cat /proc/pandora/sys_mhz_max)
 if [ "$var2" = "332" ]; then
 	message2="It has a 600Mhz OMAP processor."
 else
 	message2="It has a 1Ghz OMAP processor."
 fi
 
+# get the total RAM size. 
 var3=$(grep MemTotal /proc/meminfo | awk '{print $2}')
 ramsize=$((var3/1024))
 
@@ -20,4 +22,5 @@ else
 	message="Your Pandora is a Rebirth Edition, equipped with 512Mb of RAM."
 fi
 
+# Displays the info with a super simple Zenity line.
 zenity --info --text "$(echo $message $message2 $var0)" --title "Pandora Model Check"
