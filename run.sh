31c3c0f189d7603fcaff1bbf3eed9820acded49f
ekianjo <ekianjo@gmail.com>
2014-05-16 21:24:26 +0900

    Update run.sh
    Added new information and tested the thing, it works!

--- run.sh
+++ run.sh
@@ -6,10 +6,6 @@ var=$(cat /etc/op-version | sed -n 4p)
 # get linux version
 linuxversion=$(cat /proc/version | awk '{print $3}')
 
-linuxkernel=$(cat uname -r | awk '{print $2}')
-
-defaultlang=$(set | egrep '^(LANG|LC_)' | awk '{print $1}')
-
 usershellname=&(ps -p $$ | tail -1 | awk '{ print $4 }')
 
 var0="The latest firmware reflashed on your Pandora is $var (running Linux $linuxversion)."
@@ -29,6 +25,11 @@ fi
 var3=$(grep MemTotal /proc/meminfo | awk '{print $2}')
 ramsize=$((var3/1024))
 
+cpuspeed=$(grep BogoMIPS cat /proc/cpuinfo | awk '{print $3}')
+cpuspeed+=" Mhz"
+
+hardware=$(grep Hardware cat /proc/cpuinfo | awk '{print $3,$4,$5}')
+
 if [ $ramsize -gt 256 ] && [ $var2 -gt 390 ]; then
 	model=( "1Ghz Model (2012)" )
 else
@@ -41,9 +42,11 @@ fi
 fi
 fi	
 
+ramsize="$ramsize Mb"
 
-
+uptime=$( uptime | awk '{print $1}' )
+uptime="$uptime hours/mins/secs" 
 # Displays the info with a super simple Zenity line.
 #zenity --info --text "$(echo $message $message2 $var0)" --title "Pandora Model Check"
 
-./yad --width=400 --height=300 --title "Pandora Model Check" --list --column="Component" --column="Value" "Firmware" "$var" "Linux Version" "$linuxversion" "Linux Kernel" "$linuxkernel" "System Language" "$defaultlang" "Processor" "$processor" "RAM Size" "$var3" "Pandora Model" "${model[@]}" "User Name" "$usershellname"
+./yad --width=550 --height=330 --button=gtk-ok:1 --title "Pandora Model Check" --list --column="Component" --column="Value" "Hardware" "$hardware" "Model" "${model[@]}" "Processor" "$processor" "CPU Speed" "$cpuspeed" "RAM Size" "$ramsize" "Firmware" "$var" "Linux Version" "$linuxversion" "User Name" "$(whoami)" "System Uptime" "$uptime"
